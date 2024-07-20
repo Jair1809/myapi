71dc85f1e78d8d6321c5b3c749d752c6025d16be
Jair Dias <jairdias4@gmail.com>
2024-07-20 19:26:14 -0300

    Subindo tratamento de erros

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,10 @@
+import { AppError } from '@shared/errors/AppErrors'
 import { Router } from 'express'
+
 const routes = Router()
 
 routes.get('/', (request, response) => {
+  throw new AppError('Acesso NEGADO')
   return response.json({ message: 'ola dev' })
 })
 
