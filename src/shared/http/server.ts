5fb8af729ecc9369e5f42fb471f0d187bdf34674
Jair Dias <jairdias4@gmail.com>
2024-07-20 18:07:45 -0300

    Adicionando cors e express

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,15 @@
-export class AppServer {
-  private app: string
+import express from 'express'
+import 'express-async-errors'
+import cors from 'cors'
 
-  constructor(info: string) {
-    this.app = info ?? 'ola dev'
-  }
-}
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'ola dev' })
+})
+
+app.listen(3000, () => {
+  console.log('O SERVER ESTÁ ATIVO EM PORTA 3000!')
+})
